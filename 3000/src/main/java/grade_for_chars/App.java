44a8635278e155ff92d094f62437bdf38efe4911
Daniel Ermakov <danielermakov2012@gmail.com>
2024-01-31 19:50:06 +0200

    version 2.0 Replaced old version.

---
+++ 3000/src/main/java/grade_for_chars/App.java
@@ -0,0 +1,36 @@
+package grade_for_chars;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static String returnAGrade(int numbericGrade) {
+		if(numbericGrade >= 1 && numbericGrade <=49 ) {
+			return "F";
+		}
+		if(numbericGrade >= 50 && numbericGrade <=59 ) {
+			return "E";
+		}
+		if(numbericGrade >= 60 && numbericGrade <=69 ) {
+			return "D";
+		}
+		if(numbericGrade >= 70 && numbericGrade <=79 ) {
+			return "C";
+		}
+		
+		if(numbericGrade >= 80 && numbericGrade <=89 ) {
+			return "B";
+		}
+		if(numbericGrade >= 90 && numbericGrade <=100 ) {
+			return "A";
+		}
+		return "Error";
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println(returnAGrade(61));
+    }
+}
